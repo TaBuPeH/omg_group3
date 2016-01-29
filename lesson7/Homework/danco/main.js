9b60ad7b1a4b245093342a3838ad7a27ff451845
dan4inatora <tolev98@abv.bg>
2016-01-29 17:37:19 +0200

    da

--- lesson7/Homework/danco/main.js
+++ lesson7/Homework/danco/main.js
@@ -1,18 +1,20 @@
 var iztegleni = Array();
 
-var number = parseInt(Math.random()*42) + 1;
+var number = Math.floor(Math.random()*42) + 1;
 
 
 while(iztegleni.length<6)
 {
 
-	var number = parseInt(Math.random()*42) + 1;
+	var number = Math.floor(Math.random()*42) + 1;
 
 	if(iztegleni.indexof(number) == 1)
 	{
 
 		iztegleni[iztegleni.length]= number;
+		
 
 	}
-console.log(number)
+
 }
+iztegleni.sort()
