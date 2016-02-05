170204760c7aa84959642e7622cc3d65e2dd12bb
dan4inatora <tolev98@abv.bg>
2016-02-05 16:16:46 +0200

    s

---
+++ lesson9/homework/Dancho/JS/main.js
@@ -0,0 +1,52 @@
+var index;
+$(document).ready(function(){
+	$('.circle').click(function(){
+
+		if ($(':animated').length || $('.gallery img:visible').index()  ==  $(this).index()) 
+		{
+        	return false;
+    	}
+
+    
+    
+   
+		if($(this).index() > $('.gallery img:visible').index())
+		{
+			$('.gallery img:visible').animate( {left:"-100%"} , 1000, function(){ 
+
+				$(this).css( {left: "100%", display:"none"} );		
+			});
+					
+			var currentNumber = $(this).index();
+			var nextImage = $('.gallery img').eq(currentNumber);
+			nextImage.css('display','block');
+			nextImage.animate({left:"0%"}, 1000);
+		}
+
+	
+		if($(this).index() < $('.gallery img:visible').index())
+		{
+			$('.gallery img:visible').animate( {left:"100%"} , 1000, function(){ 
+
+				$(this).css( {left: "100%", display:"none"} );
+			});
+			var currentNumber = $(this).index();
+			var nextImage = $('.gallery img').eq(currentNumber);
+			nextImage.css('left','-100%');
+			nextImage.css('display','block');
+			nextImage.animate({left:"0%"}, 1000);
+					
+	
+		}
+
+		
+		
+
+	});
+	
+
+});
+
+
+
+
