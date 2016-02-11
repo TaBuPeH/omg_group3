6ca598e7e5f3f22cc05097e7c43bb4ed14b1e456
Georgi Peshev <georgi.peshev@stanga.net>
2016-02-11 14:03:59 +0200

    a

--- lesson10/lesson/js/gallery.js
+++ lesson10/lesson/js/gallery.js
@@ -11,11 +11,11 @@ $(document).ready(function(){
 			
 			if(activeIndex < ourIndex)
 				{
-					flyInFromRight(500);
+					flyInFromRight(300);
 				}
 			else
 				{
-					flyInFromLeft(500);
+					flyInFromLeft(300);
 				}
 			
 		}
@@ -27,12 +27,15 @@ $(document).ready(function(){
 	function flyInFromRight(delay)
 	{
 		 var currentElement = $('.gallery img.active');
+		 
+		 
 		 setTimeout(function(){ 
 			 currentElement.removeClass('active'); 
 			 currentElement.next().addClass('active'); 
-		 },delay/2);
+		 },delay/2 - 50);
 		 
-		 currentElement.prev().animate({width:'15%',top:'35%',left: "-15%"});
+		 
+		 currentElement.prev().animate({width:'15%',top:'35%',left: "-15%"}, delay-50);
 		 currentElement.animate( {left: "0px", top: "15%", width: '35%'}, delay );
 		 currentElement.next().animate( {left: "25%", top: "0", width: "50%"}, delay);
 		 currentElement.next().next().animate( {left: "70%", top: "15%", width: "35%"}, delay);
@@ -46,42 +49,14 @@ $(document).ready(function(){
 			 currentElement.prev().addClass('active'); 
 		 },delay/2 - 50);
 		 
-		 currentElement.next().animate({width:'15%',top:'35%',left: "100%"});
-		 currentElement.animate( {left: "70%", top: "15%", width: '35%'}, delay- 50 );
-		 currentElement.prev().animate( {left: "25%", top: "0", width: "50%"}, delay- 50);
-		 currentElement.prev().prev().animate( {left: "0%", top: "15%", width: "35%"}, delay- 50);
+		
+		 currentElement.next().animate({width:'15%',top:'35%',left: "100%"}, delay-50);
+		 currentElement.animate( {left: "70%", top: "15%", width: '35%'}, delay );
+		 currentElement.prev().animate( {left: "25%", top: "0", width: "50%"}, delay);
+		 currentElement.prev().prev().animate( {left: "0%", top: "15%", width: "35%"}, delay);
 	}
 	
 	
 	
 	
-    $( "#ourSlider" ).slider({
-        min: 0,
-        max: 7,
-        value: 0,
-        slide: function( event, ui ) {
-        	 var currentElement = $('.gallery img.active').index();
-        	 var currentPicture = ui.value;
-        	 
-        	 
-        	 var numberOfElements= Math.abs(currentPicture - currentElement);
-        	
-        	 var delay = 500/numberOfElements; // 250msec
-        	 
-        	 if(currentElement<currentPicture)
-        	 {	
-        		 for(i=0; i<numberOfElements;i++)
-	        	 { 
-	        		 setTimeout(flyInFromRight,delay*i,delay);
-	        	 }
-        	 }
-        	 else
-             {
-        		 for(i=0; i<numberOfElements;i++)
-	        	 { 
-	        		 setTimeout(flyInFromLeft,delay*i,delay);
-	        	 }
-        	 }
-        }
-      });
 });
