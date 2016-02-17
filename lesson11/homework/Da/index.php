54a64a5f042c93770d07d32707789b00ec50827e
dan4inatora <tolev98@abv.bg>
2016-02-17 22:48:02 +0200

    das ist eine comrade

---
+++ lesson11/homework/Da/index.php
@@ -0,0 +1,50 @@
+<?php
+			$Pigon = Array(
+				1 => Array(
+					"name" => "mikasa",
+					"age" => 3,
+					"price" => "20.00$",
+					"description" => Array(
+						"Appearance" => "Black & yellow",
+						"Special abilities" => Array(
+							0 => "ninja",
+							1 => "can fly",
+							2 => "can do a 30 sign jutsu"
+						)
+					)
+				),
+
+					2 => Array(
+					"name" => "levi",
+					"age" => 4,
+					"price" => "2099.00$",
+					"description" => Array(
+						"Appearance" => "Black & yellow",
+						"Special abilities" => Array(
+							0 => "can kill with only looking",
+							1 => "can barrel roll",
+							
+						)
+					)
+				),
+
+					3 => Array(
+					"name" => "eren",
+					"age" => 5,
+					"price" => "209.00$",
+					"description" => Array(
+						"Appearance" => "Black & yellow",
+						"Special abilities" => Array(
+							0 => "can go invisible",
+							1 => "can fly",
+							2 => "can speak"
+						)
+					)
+				),
+			);
+
+			echo "<pre>";
+			print_r($Pigon);
+			echo "</pre>";
+
+		?>
